0c27769137e0efcdb2b7239304ee41947b8199a2
Dale Seo <dale.seo@gmail.com>
2017-05-02 13:41:05 +0900

    EngTable Wireframe

--- public/src/components/EngTable.vue
+++ public/src/components/EngTable.vue
@@ -1,6 +1,18 @@
 <template>
   <div class="container">
-    Test
+    <b-table hover
+      :items="items"
+      :fields="fields"
+      :current-page="currentPage"
+      :per-page="perPage"
+      :filter="filter">
+      <template slot="name" scope="item">
+        {{item.value.first}} {{item.value.last}}
+      </template>
+      <template slot="actions" scope="item">
+        <b-btn size="sm" @click="details(item.item)">Details</b-btn>
+      </template>
+    </b-table>
   </div>
 </template>
 
@@ -8,6 +20,127 @@
 export default {
   data () {
     return {
+      currentPage: 1,
+      perPage: 5,
+      filter: null,
+      fields: {
+        name: {
+          label: 'Person Full name',
+          sortable: true
+        },
+        age: {
+          label: 'Person age',
+          sortable: true
+        },
+        isActive: {
+          label: 'is Active'
+        },
+        actions: {
+          label: 'Actions'
+        }
+      },
+      items: [{
+        isActive: true,
+        age: 40,
+        name: {
+          first: 'Dickerson',
+          last: 'Macdonald'
+        }
+
+      }, {
+        isActive: false,
+        age: 21,
+        name: {
+          first: 'Larsen',
+          last: 'Shaw'
+        }
+
+      }, {
+        isActive: false,
+        age: 9,
+        state: 'success',
+        name: {
+          first: 'Mitzi',
+          last: 'Navarro'
+        }
+
+      }, {
+        isActive: false,
+        age: 89,
+        name: {
+          first: 'Geneva',
+          last: 'Wilson'
+        }
+
+      }, {
+        isActive: true,
+        age: 38,
+        name: {
+          first: 'Jami',
+          last: 'Carney'
+        }
+
+      }, {
+        isActive: false,
+        age: 27,
+        name: {
+          first: 'Essie',
+          last: 'Dunlap'
+        }
+
+      }, {
+        isActive: true,
+        age: 40,
+        name: {
+          first: 'Dickerson',
+          last: 'Macdonald'
+        }
+
+      }, {
+        isActive: false,
+        age: 21,
+        name: {
+          first: 'Larsen',
+          last: 'Shaw'
+        }
+
+      }, {
+        isActive: false,
+        age: 26,
+        name: {
+          first: 'Mitzi',
+          last: 'Navarro'
+        }
+
+      }, {
+        isActive: false,
+        age: 22,
+        name: {
+          first: 'Geneva',
+          last: 'Wilson'
+        }
+
+      }, {
+        isActive: true,
+        age: 38,
+        name: {
+          first: 'Jami',
+          last: 'Carney'
+        }
+
+      }, {
+        isActive: false,
+        age: 27,
+        name: {
+          first: 'Essie',
+          last: 'Dunlap'
+        }
+      }]
+    }
+  },
+  methods: {
+    details(item) {
+      alert(JSON.stringify(item));
     }
   }
 }
--- public/src/main.js
+++ public/src/main.js
@@ -1,8 +1,10 @@
 import Vue from 'vue'
 import BootstrapVue from 'bootstrap-vue'
+import VueResource from 'vue-resource'
 import App from './App.vue'
 
 Vue.use(BootstrapVue)
+Vue.use(VueResource)
 
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
